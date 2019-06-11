c88ccd0ed0b9156bbdeb165cc5d82c74f9d0e8d7
Gumbx <gustavo.bonassa1@gmail.com>
2019-06-11 18:15:47 -0300

    images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-n4nnb.mongodb.net/ig?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
+
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads','resized')));
 
 app.use(require('./routes'));
 
