e78a8f5f932063528a8c021ac9361332d649bd40
Gumbx <gustavo.bonassa1@gmail.com>
2019-06-12 08:42:21 -0300

    socket e cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-n4nnb.mongodb.net/ig?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
+app.use((req,res,next)=>{
+    req.io = io;
+    next();
+});
+
+app.use(cors());
+
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads','resized')));
 
 app.use(require('./routes'));
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
