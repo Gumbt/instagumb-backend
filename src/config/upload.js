30e6e946dcb64d4d39d4bfd2363f31eb50fc6ac6
Gumb <gustavo.bonassa1@gmail.com>
2019-06-12 21:29:31 -0300

    back

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,40 +1,19 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
-const aws = require('aws-sdk');
-const multerS3 = require('multer-s3');
 
-const storageTypes = {
-    local: new multer.diskStorage({
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'uploads'),
+    storage: new multer.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
         filename: function (req, file, cb) {
-            crypto.randomBytes(16, (err, hash) => {
-                if (err) cb(err);
-
-                file.key = `${hash.toString('hex')}-${file.originalname}}`;
-
-                cb(null, file.key);
-            });
-        }
-    }),
-    s3: multerS3({
-        s3: new aws.S3(),
-        bucket: 'uploadgumb',
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'public-read',
-        key: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
                 if (err) cb(err);
 
                 const fileName = `${hash.toString('hex')}-${file.originalname}}`;
 
                 cb(null, fileName);
-            });
+            })
         }
     })
-}
-
-module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'uploads'),
-    storage: storageTypes["s3"],
 };
